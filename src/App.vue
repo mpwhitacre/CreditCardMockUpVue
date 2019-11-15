33cac6ac09fec8782f6315d49fa3665a2e844622
mpwhitacre <devuser@ip-10-12-34-126.primrosenet.net>
2019-11-15 08:51:08 -0500

    this will overwrite our old project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+       <CreditCard/>
+       <GitLink/>
+  </div>
+</template>
+
+<script>
+import CreditCard from './components/CreditCard/CreditCard.vue'
+import GitLink from './components/Github/GitHubLink.vue'
+
+export default {
+ name: 'app',
+ components: {
+   CreditCard,
+   GitLink
+ }
+}
+</script>
---
+++ src/CreditCard.js
@@ -0,0 +1,7 @@
+
+export function creditCard (val1) {
+  return "hey: "+val1;
+}
+export function creditCard2 (val1) {
+  return "no: "+val1;
+}
